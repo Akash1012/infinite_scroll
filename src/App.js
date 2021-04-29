a96b18c901605e3924e3a5c25c99eb4a54b27a17
Akash1012 <gupta.akash2090@gmail.com>
2021-04-29 18:02:20 +0530

    Complete the Task

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useCallback } from 'react';
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import CardList from './Component/cardList'
+
+
+const App = () => {
+    const [data, setData] = useState([]);
+    const [query, setQuery] = useState({ start: 0, limit: 10 })
+
+    useEffect(() => {
+        getApiData()
+    }, [query.start])
+
+    const getApiData = () => {
+        axios
+            .get(
+                `http://jsonplaceholder.typicode.com/photos?_start=${query.start}&_limit=${query.limit}`
+            )
+            .then((res) => {
+                setData([...data, ...res.data])
+            })
+            .catch((e) => {
+                console.error(e)
+            })
+    }
+
+    const set = (query) => {
+        setQuery(query)
+    }
+
+    return (
+        <div>
+            <h1><i>LIST OF CARDS</i></h1>
+            <CardList listOfCard={data} set={set} getApiData={getApiData} />
+        </div>
+    )
 }
 
 export default App;
